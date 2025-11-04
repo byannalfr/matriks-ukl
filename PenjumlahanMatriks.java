1a075c8d530a581d5e4c3bad2722e97ba4d4fc39
byannalfr <muhammad_altafianto_34rpl@student.smktelkom-mlg.sch.id>
2025-11-04 12:31:02 +0700

    Add files via upload

---
+++ PenjumlahanMatriks.java
@@ -0,0 +1,56 @@
+package byannalfr;
+
+import java.util.Scanner;
+
+public class PenjumlahanMatriks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Meminta input dimensi matriks
+        System.out.print("Masukkan jumlah baris matriks: ");
+        int baris = scanner.nextInt();
+        System.out.print("Masukkan jumlah kolom matriks: ");
+        int kolom = scanner.nextInt();
+        
+        int[][] matriks1 = new int[baris][kolom];
+        int[][] matriks2 = new int[baris][kolom];
+        int[][] hasil = new int[baris][kolom];
+        
+        // Input matriks pertama
+        System.out.println("Masukkan elemen matriks pertama:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print("Elemen [" + i + "][" + j + "]: ");
+                matriks1[i][j] = scanner.nextInt();
+            }
+        }
+        
+        // Input matriks kedua
+        System.out.println("Masukkan elemen matriks kedua:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print("Elemen [" + i + "][" + j + "]: ");
+                matriks2[i][j] = scanner.nextInt();
+            }
+        }
+        
+        // Penjumlahan matriks
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                hasil[i][j] = matriks1[i][j] + matriks2[i][j];
+            }
+        }
+        
+        // Menampilkan hasil penjumlahan
+        System.out.println("Hasil penjumlahan matriks:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print(hasil[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        // Menutup scanner
+        scanner.close();
+    }
+}
